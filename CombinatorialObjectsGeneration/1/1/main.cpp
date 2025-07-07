6775277bbd85fb4bbf287923002c9ab5328a5909
YelKar <karamyshev_e@1060.ru>
2025-07-07 15:52:08 +0300

    Обновил таймер

--- CombinatorialObjectsGeneration/1/1/main.cpp
+++ CombinatorialObjectsGeneration/1/1/main.cpp
@@ -1,29 +1,13 @@
-#include <algorithm>
 #include "timer.h"
 #include "print.h"
-#include "matrix.h"
 #include "Permutations.h"
 
-#include <limits>
 
 int main(int argc, char *argv[]) {
-    tools::Timer timer;
 	std::vector<int> array = Permutations::First(13);
-	timer.Start();
-	do {
-//		tools::PrintArray(array);
-	} while (Permutations::Next(array));
-	auto time = timer.GetDelta();
-	std::cout << "Time for N = " << array.size() << ": " << time.ToSeconds() << "s" << std::endl;
-//	for (int i = array.size(); i <= 15; i++) {
-//		array.push_back(i);
-//	}
-//	timer.start();
-//	do {
-//		// tools::PrintArray(array);
-//	} while (Narayana::NextPermutation(array));
-//	time = timer.getDelta();
-//	std::cout << "Time for N = " << array.size() << ": " << time.toSeconds() << "s" << std::endl;
+	auto time = tools::LoggingTimer::Measure("Time for N = " + std::to_string(array.size()), [&array] {
+		while (Permutations::Next(array));
+	});
 	std::cout << "Estimated time for N = 20: " << time.ToSeconds() * (tgamma(20) / tgamma(array.size() + 1)) << "s" << std::endl;
 	std::cout << "Estimated time for N = 50: " << time.ToSeconds() * (tgamma(50) / tgamma(array.size() + 1)) << "s" << std::endl;
     return 0;
--- tools/timer.h
+++ tools/timer.h
@@ -1,20 +1,22 @@
 #pragma once
 #include <chrono>
 #include <cmath>
+#include <iostream>
+#include <utility>
 
 namespace tools {
     class Delta {
     private:
-        double nanoseconds;
+	    std::int64_t nanoseconds;
 
     public:
         Delta() : nanoseconds(0) {}
-        explicit Delta(double ns) : nanoseconds(ns) {}
+        explicit Delta(std::int64_t ns) : nanoseconds(ns) {}
 
-        [[nodiscard]] double ToNanoseconds() const { return nanoseconds; }
-        [[nodiscard]] double ToMicroseconds() const { return nanoseconds / 1e3; }
-        [[nodiscard]] double ToMilliseconds() const { return nanoseconds / 1e6; }
-        [[nodiscard]] double ToSeconds() const { return nanoseconds / 1e9; }
+        [[nodiscard]] std::int64_t ToNanoseconds() const { return nanoseconds; }
+        [[nodiscard]] double ToMicroseconds() const { return static_cast<double>(nanoseconds) / 1e3; }
+        [[nodiscard]] double ToMilliseconds() const { return static_cast<double>(nanoseconds) / 1e6; }
+        [[nodiscard]] double ToSeconds() const { return static_cast<double>(nanoseconds) / 1e9; }
         [[nodiscard]] double ToMinutes() const { return ToSeconds() / 60; }
         [[nodiscard]] double ToHours() const { return ToMinutes() / 60; }
         [[nodiscard]] double ToDays() const { return ToHours() / 24; }
@@ -22,16 +24,7 @@ namespace tools {
 
         [[nodiscard]] std::string ToString() const {
             std::ostringstream oss;
-            oss << "Delta {\n"
-                << "    nanoseconds = " << ToNanoseconds() << "\n"
-                << "    microseconds = " << ToMicroseconds() << "\n"
-                << "    milliseconds = " << ToMilliseconds() << "\n"
-                << "    seconds = " << ToSeconds() << "\n"
-                << "    minutes = " << ToMinutes() << "\n"
-                << "    hours = " << ToHours() << "\n"
-                << "    days = " << ToDays() << "\n"
-                << "    years = " << ToYears() << "\n"
-                << "}";
+            oss << "Delta(" << nanoseconds << "ns)";
             return oss.str();
         }
 
@@ -46,25 +39,74 @@ namespace tools {
             this->nanoseconds *= num;
         }
 
-		double& operator *() {
+	    std::int64_t& operator *() {
 			return this->nanoseconds;
 		}
 
-		double operator *() const {
+	    std::int64_t operator *() const {
 			return this->nanoseconds;
 		}
     };
 
-    class Timer {
-        std::chrono::high_resolution_clock::time_point _start;
-    public:
-        void Start() {
-            _start = std::chrono::high_resolution_clock::now();
-        }
+	class Timer {
+		using Clock = std::chrono::high_resolution_clock;
+	public:
+		void Restart() {
+			m_start = Clock::now();
+			m_running = true;
+		}
+		Delta GetDelta() {
+			return Delta((Clock::now() - m_start).count());
+		}
+		virtual Delta Stop() {
+			m_running = false;
+			return GetDelta();
+		}
 
-        Delta GetDelta() {
-            Delta delta(static_cast<double>((std::chrono::high_resolution_clock::now() - _start).count()));
-            return delta;
-        }
-    };
+		[[nodiscard]] bool IsRunning() const {
+			return m_running;
+		}
+
+		template<typename Fn, typename... Args>
+		static Delta Measure(Fn&& fn, Args&&... args) {
+			Timer timer;
+			fn(std::forward<Args>(args)...);
+			return timer.GetDelta();
+		}
+	private:
+		Clock::time_point m_start = Clock::now();
+		bool m_running = true;
+	};
+
+	class LoggingTimer : public Timer {
+		using Clock = std::chrono::high_resolution_clock;
+	public:
+		explicit LoggingTimer(std::string name)
+			: m_name(std::move(name))
+		{
+		}
+		~LoggingTimer() {
+			LoggingTimer::Stop();
+		}
+		Delta Stop() override {
+			if (IsRunning()) {
+				auto delta = Timer::Stop();
+				std::cout << m_name << ": " << delta.ToSeconds() << "s" << std::endl;
+				return delta;
+			}
+			return Delta(0);
+		}
+		[[nodiscard]] std::string GetName() const {
+			return m_name;
+		}
+
+		template<typename Fn, typename... Args>
+		static Delta Measure(std::string name, Fn&& fn, Args&&... args) {
+			LoggingTimer timer(std::move(name));
+			fn(std::forward<Args>(args)...);
+			return timer.Stop();
+		}
+	private:
+		std::string m_name;
+	};
 }
