6f65095ad92dd3b8b4bf1367088c0333a7c5d70e
YelKar <karamyshev_e@1060.ru>
2025-07-07 18:58:52 +0300

    Обновил таймер

--- CombinatorialObjectsGeneration/1/2/main.cpp
+++ CombinatorialObjectsGeneration/1/2/main.cpp
@@ -4,43 +4,53 @@
 #include "matrix.h"
 #include "TravelingSalesman.h"
 
+bool ProcessArgs(int argc, char *argv[], int &countOfPoints, std::vector<std::vector<int>> &distanceMtx);
+
 int main(int argc, char *argv[]) {
-	tools::Timer timer;
 	int countOfPoints = 0;
 	std::vector<std::vector<int>> distanceMtx;
 
-	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " <count of cities> <volume matrix file>" << std::endl;
+	if (!ProcessArgs(argc, argv, countOfPoints, distanceMtx)) {
 		return 1;
-	} else {
-		countOfPoints = std::stoi(argv[1]);
-		tools::ParseMatrix(argv[2], distanceMtx);
-	}
-	if (distanceMtx.size() < countOfPoints) {
-		std::cerr << "Error: distanceMtx must have " << countOfPoints << " rows or more" << std::endl;
-		return 1;
-	}
-
-	distanceMtx.resize(countOfPoints);
-	for (int i = 0; i < countOfPoints; i++) {
-		distanceMtx[i].resize(countOfPoints);
 	}
 
 	std::cout << "Cities:" << std::endl;
 	tools::PrintMatrix(distanceMtx);
 
+	std::vector<int> path(countOfPoints);
+	std::iota(path.begin(), path.end(), 0);
 
-	std::vector<int> path = {};
-	for (int i = 0; i < countOfPoints; i++) {
-		path.push_back(i);
-	}
-	timer.Start();
-	int length = FindShortestPath(distanceMtx, path);
-	auto time = timer.GetDelta();
+	int length;
+	auto time = tools::Timer::Measure([&length, distanceMtx, &path] {
+		length = FindShortestPath(distanceMtx, path);
+	});
+
 	std::cout << "Path length: " << length << std::endl;
 	std::cout << "path: ";
 	tools::PrintArray(path);
 	std::cout << "Time: " << time.ToSeconds() << "s" << std::endl;
 	return 0;
 }
+
+
+bool ProcessArgs(int argc, char *argv[], int &countOfPoints, std::vector<std::vector<int>> &distanceMtx) {
+	if (argc != 3) {
+		std::cerr << "Usage: " << argv[0] << " <count of cities> <volume matrix file>" << std::endl;
+		return false;
+	} else {
+		countOfPoints = std::stoi(argv[1]);
+		tools::ParseMatrix(argv[2], distanceMtx);
+	}
+	if (distanceMtx.size() < countOfPoints) {
+		std::cerr << "Error: distanceMtx must have " << countOfPoints << " rows or more" << std::endl;
+		return false;
+	}
+
+	distanceMtx.resize(countOfPoints);
+	for (int i = 0; i < countOfPoints; i++) {
+		distanceMtx[i].resize(countOfPoints);
+	}
+	return true;
+}
+
 
--- CombinatorialObjectsGeneration/3/2/main.cpp
+++ CombinatorialObjectsGeneration/3/2/main.cpp
@@ -4,37 +4,47 @@
 #include "matrix.h"
 #include "GraphColoring.h"
 
+
+bool ProcessArgs(int argc, char **argv, int &countOfPoints, std::vector<std::vector<int>> &adjacencyMtx);
+
 int main(int argc, char** argv) {
-	tools::Timer timer;
 	int countOfPoints = 0;
 	std::vector<std::vector<int>> adjacencyMtx;
 
-	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " <count of vertexes> <matrix file>" << std::endl;
+	if (!ProcessArgs(argc, argv, countOfPoints, adjacencyMtx)) {
 		return 1;
-	} else {
-		countOfPoints = std::stoi(argv[1]);
-		tools::ParseMatrix(argv[2], adjacencyMtx);
-	}
-	if (adjacencyMtx.size() < countOfPoints) {
-		std::cerr << "Error: distanceMtx must have " << countOfPoints << " rows or more" << std::endl;
-		return 1;
-	}
-
-	adjacencyMtx.resize(countOfPoints);
-	for (int i = 0; i < countOfPoints; i++) {
-		adjacencyMtx[i].resize(countOfPoints);
 	}
 
 	std::cout << "Adjacency Matrix:" << std::endl;
 	tools::PrintMatrix(adjacencyMtx);
-
-	timer.Start();
-	auto colors = ColorGraph(adjacencyMtx);
-	auto time = timer.GetDelta();
+	std::vector<int> colors;
+	auto time = tools::Timer::Measure([&colors, &adjacencyMtx] {
+		colors = ColorGraph(adjacencyMtx);
+	});
 	std::cout << "Colors (" << *std::max_element(colors.begin(), colors.end()) + 1 << "):" << std::endl;
 	tools::PrintArray(colors);
 	std::cout << "Time: " << time.ToSeconds() << "s" << std::endl;
 
 	return 0;
 }
+
+bool ProcessArgs(int argc, char **argv, int &countOfPoints, std::vector<std::vector<int>> &adjacencyMtx) {
+	if (argc != 3) {
+		std::cerr << "Usage: " << argv[0] << " <count of vertexes> <matrix file>" << std::endl;
+		return false;
+	} else {
+		countOfPoints = std::stoi(argv[1]);
+		tools::ParseMatrix(argv[2], adjacencyMtx);
+	}
+	if (adjacencyMtx.size() < countOfPoints) {
+		std::cerr << "Error: distanceMtx must have " << countOfPoints << " rows or more" << std::endl;
+		return false;
+	}
+
+	adjacencyMtx.resize(countOfPoints);
+	for (int i = 0; i < countOfPoints; i++) {
+		adjacencyMtx[i].resize(countOfPoints);
+	}
+	return true;
+}
+
